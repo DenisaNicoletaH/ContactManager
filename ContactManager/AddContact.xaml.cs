5b91556ef7e28a91c6fcc1f0c1e1922dea357e70
Caleb <liucaleb8@gmail.com>
2022-12-10 11:25:47 -0500

    add contact with validation

--- ContactManager/AddContact.xaml.cs
+++ ContactManager/AddContact.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
+using System.Collections;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace ContactManager
 {
@@ -33,15 +35,74 @@ namespace ContactManager
             //incomplete
             using (SqlConnection con = new SqlConnection(connectionString))
             {
+                string fName = tb1.Text;
+                string lName = tb2.Text;
+                string mName = tb3.Text;
+                string active = tb4.Text;
+                string iId = tb5.Text;
+                int imageId=-1;
+                ArrayList imageIds = new ArrayList();
+
+                if (fName.Equals("") || lName.Equals("") || active.Equals("") ||
+                    mName.Equals("") || iId.Equals(""))
+                {
+                    MessageBox.Show("One or more of the fields above is empty");
+                    return;
+                }
+
+                if (mName.Length > 1)
+                {
+                    MessageBox.Show("Middle name can only be one character");
+                    return;
+                }
+
+                if (!active.ToLower().Equals("true") && !active.ToLower().Equals("false"))
+                {
+                    MessageBox.Show("Active field is not valid! Please enter true or false");
+                    return;
+                }
+
+                bool validId = Int32.TryParse(iId,out imageId);
+                if (!validId || imageId<=0)
+                {
+                    MessageBox.Show("Image id is not a valid number");
+                    return;
+                }
+
+                using (SqlConnection con2 = new SqlConnection(connectionString))
+                {
+                    con2.Open();
+                    SqlCommand cm = new SqlCommand("select Id from Contact_Image", con2);
+                    SqlDataReader sdr = cm.ExecuteReader();
+                    while (sdr.Read())
+                    {
+                        imageIds.Add(Int32.Parse(sdr["Id"].ToString()));
+                    }
+                }
+
+                bool isExist = false;
+                foreach (int i in imageIds)
+                {
+                    if (imageId == i)
+                    { 
+                        isExist = true;
+                    }
+                }
+                if (!isExist)
+                {
+                    MessageBox.Show("Image id is not valid");
+                    return;
+                }
+                DateTime currentTime=DateTime.Now;
                 con.Open();
-                SqlCommand command = new SqlCommand("INSERT INTO Contact(FirstName,LastName,Active,CreatedDate,UpdatedDate,MiddleName,Image_Id) VALUES(@firstName,@lastName,@active,@createdDate,@updatedDate,@middleName,@imageId)", con);
-                command.Parameters.AddWithValue("@firstName",tb1.Text);
-                command.Parameters.AddWithValue("@lastName", tb2.Text);
-                command.Parameters.AddWithValue("@active", tb3.Text);
-                command.Parameters.AddWithValue("@createdDate", tb4.Text);
-                command.Parameters.AddWithValue("@updatedDate", tb5.Text);
-                command.Parameters.AddWithValue("@middleName", tb6.Text);
-                command.Parameters.AddWithValue("@imageId", tb7.Text);
+                SqlCommand command = new SqlCommand("INSERT INTO Contact(FirstName,LastName,MiddleName,Active,Image_Id,CreateDate,UpdateDate) VALUES(@firstName,@lastName,@middleName,@active,@imageId,@createDate,@updateDate)", con);
+                command.Parameters.AddWithValue("@firstName",fName);
+                command.Parameters.AddWithValue("@lastName", lName);
+                command.Parameters.AddWithValue("@middleName", mName);
+                command.Parameters.AddWithValue("@active", active);
+                command.Parameters.AddWithValue("@imageId", iId);
+                command.Parameters.AddWithValue("@createDate", currentTime);
+                command.Parameters.AddWithValue("@updateDate", currentTime);
                 command.ExecuteNonQuery();
             }
         }
