ca29f46e19c93aba51d1b83a3c9b8b2c8ff0afe9
Mila5847 <46633364+Mila5847@users.noreply.github.com>
2022-12-14 22:58:58 -0500

    creation fo delete and update functionality

--- ContactManager/MainWindow.xaml.cs
+++ ContactManager/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,12 +32,16 @@ namespace ContactManager
 
             DB dB = new DB();
             var contacts = dB.GetContacts();
+            // var phoneNumbers = dB.getPhoneForContact();
             foreach (var contact in contacts)
             {
-                this.listView.Items.Add(new Contact {Id=contact.Id, FirstName = contact.FirstName, LastName=contact.LastName});
+                this.listView.Items.Add(new Contact { Id = contact.Id, FirstName = contact.FirstName, LastName = contact.LastName, /*MiddleName = contact.MiddleName */ });
+
             }
+
         }
 
+        //Double Click The Contact
         private void listView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             var contact = listView.SelectedItem as Contact;
@@ -45,5 +50,16 @@ namespace ContactManager
             detailsWindow.Show();
             detailsWindow.Focus();
         }
+
+        //Add Contact Button
+        /*private void AddContact_Click(object sender, RoutedEventArgs e)
+        {
+            AddContact detailsWindow = new AddContact();
+            detailsWindow.Show();
+            detailsWindow.Focus();
+        }*/
+    
+
+      
     }
 }
