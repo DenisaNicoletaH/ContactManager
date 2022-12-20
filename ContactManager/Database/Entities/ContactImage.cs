49c2ef1c26daa471e7526244d51622755a7629cf
Caleb <liucaleb8@gmail.com>
2022-12-20 16:10:42 -0500

    Contact Image Entity

---
+++ ContactManager/Database/Entities/ContactImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Database.Entities
+{
+    internal class ContactImage
+    {
+        public int Id { get; set; }
+        public string ImagePath { get; set; }
+        public byte[] ImageToByte { get; set; }
+        public string Description { get; set; }
+    }
+}
